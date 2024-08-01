38f397321405d66e6da0683701a80afeef6b8858
Andre Arias <aarias683@gmail.com>
2024-07-31 23:57:58 -0400

    Update homepage_clicks.sql

--- GA4_ecommerce_reports/homepage_clicks.sql
+++ GA4_ecommerce_reports/homepage_clicks.sql
@@ -16,17 +16,17 @@ WITH homepage_clicks AS (
 --joining transactions and revenue with users and sessions from the homepage_clicks CTE
 SELECT
 	hc.event_category,
-    hc.event_subcategory,
-    COUNT(DISTINCT hc.user_pseudo_id) AS total_users,
-	COUNT(DISTINCT hc.session_id) AS sessions,    
-    SUM(CASE WHEN p.event_name = 'purchase' THEN 1 ELSE 0 END) AS transactions,
-    SUM(CASE WHEN p.event_name = 'purchase' THEN p.ecommerce.purchase_revenue ELSE 0 END) AS revenue
+	hc.event_subcategory,
+	COUNT(DISTINCT hc.user_pseudo_id) AS total_users,
+	COUNT(DISTINCT CONCAT(user_pseudo_id, (select value.int_value from unnest(event_params) where key = 'ga_session_id'))) as sessions,    
+	SUM(CASE WHEN p.event_name = 'purchase' THEN 1 ELSE 0 END) AS transactions,
+	SUM(CASE WHEN p.event_name = 'purchase' THEN p.ecommerce.purchase_revenue ELSE 0 END) AS revenue
 FROM
 	homepage_clicks hc
 LEFT JOIN
 	`table name` p
 ON
-    hc.user_pseudo_id = p.user_pseudo_id
-    AND hc.session_id = (SELECT value.int_value FROM UNNEST(p.event_params) WHERE key = 'ga_session_id')
+	hc.user_pseudo_id = p.user_pseudo_id
+	AND hc.session_id = (SELECT value.int_value FROM UNNEST(p.event_params) WHERE key = 'ga_session_id')
 GROUP BY
 	hc.event_category, hc.event_subcategory;
