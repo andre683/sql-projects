f2b8745130c020e9bebaedb7d4637c541bbbda55
Andre Arias <aarias683@gmail.com>
2024-07-31 23:13:31 -0400

    Update homepage_clicks.sql

--- GA4_ecommerce_reports/homepage_clicks.sql
+++ GA4_ecommerce_reports/homepage_clicks.sql
@@ -1,50 +1,31 @@
+--CTE filtering homepage_clicks event
 WITH homepage_clicks AS (
-    SELECT
-        user_pseudo_id,
-        CAST((SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS STRING) AS session_id,
-        (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'event_category') AS event_category,
-        (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'event_subcategory') AS event_subcategory,
-        PARSE_DATE('%Y%m%d', event_date) AS date
-    FROM
-        `tough-healer-395417.analytics_287163560.events_*`
-    WHERE
-        event_name = 'homepage_clicks'
-        AND geo.country != 'India'
-        AND device.web_info.hostname = 'www.laithwaites.com'
-        AND _TABLE_SUFFIX BETWEEN '20240501' AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
-),
-homepage_clicks_purchase AS (
-    SELECT
-        hc.session_id,
-        hc.user_pseudo_id,
-        hc.event_category,
-        hc.event_subcategory,
-        hc.date,
-        SUM(CASE WHEN p.event_name = 'purchase' THEN 1 ELSE 0 END) AS transactions,
-        SUM(CASE WHEN p.event_name = 'purchase' THEN p.ecommerce.purchase_revenue ELSE 0 END) AS revenue
-    FROM
-        homepage_clicks hc
-    LEFT JOIN
-        `tough-healer-395417.analytics_287163560.events_*` p
-    ON
-        hc.user_pseudo_id = p.user_pseudo_id
-        AND CAST((SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS STRING) = hc.session_id
-    WHERE
-        _TABLE_SUFFIX BETWEEN '20240501' AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
-    GROUP BY
-        hc.session_id, hc.user_pseudo_id, hc.event_category, hc.event_subcategory, hc.date
+  SELECT
+  	user_pseudo_id,
+  	(SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS session_id,
+  	--column to include all values for event_category (logged-in or logged-out)
+  	(SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'event_category') AS event_category,
+  	--column to include all values for event_subcategory (banner location)
+  	(SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'event_subcategory') AS event_subcategory,
+  FROM
+  	`tablename`
+  WHERE
+  	--filters for homepage_clicks event
+  	event_name = 'homepage_clicks'
 )
+--joining transactions and revenue with users and sessions from the homepage_clicks CTE
 SELECT
-    event_category,
-    event_subcategory,
-    date,
-    user_pseudo_id,
-    session_id,
-    SUM(transactions) AS transactions,
-    SUM(revenue) AS revenue
+	hc.event_category,
+    hc.event_subcategory,
+	COUNT(DISTINCT hc.session_id) AS sessions,
+    COUNT(DISTINCT hc.user_pseudo_id) AS total_users,
+    SUM(CASE WHEN p.event_name = 'purchase' THEN 1 ELSE 0 END) AS transactions,
+    SUM(CASE WHEN p.event_name = 'purchase' THEN p.ecommerce.purchase_revenue ELSE 0 END) AS revenue
 FROM
-    homepage_clicks_purchase
+	homepage_clicks hc
+LEFT JOIN
+	`table name` p
+ON
+    hc.user_pseudo_id = p.user_pseudo_id
+    AND (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'event_category') = hc.session_id
 GROUP BY
-    event_category, event_subcategory, date, user_pseudo_id, session_id
-ORDER BY
-    date DESC;
